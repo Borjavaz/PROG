6924fc06e0f60169a298cb97729fd25ab81cdfb8
Borja De Saa <bdesaavazquez@danielcastelao.org>
2024-10-21 14:33:05 +0200

    cuentaAtras terminada

---
+++ boletin1.1/src/cuentaAtras.java
@@ -0,0 +1,10 @@
+public class cuentaAtras{
+    public static void main(String[] args) {
+        // Bucle for que inicia en 0 y va hasta 10
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("¡Despegue!");
+    }
+}
+
