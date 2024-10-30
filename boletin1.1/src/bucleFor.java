b137866f9611ab45946fc60360ed78553230aa27
Borja De Saa <bdesaavazquez@danielcastelao.org>
2024-10-30 09:37:56 +0100

    Ejercicio con bucle for

--- boletin1.1/src/bucleFor.java
+++ boletin1.1/src/bucleFor.java
@@ -1,18 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Este programa muestra los multiplos del 7 menores que 100
  * @version 1.1
  * @author Borja de Saa
  */
 
-public class multiplos7 {
+public class bucleFor {
     public static void main(String[] args) {
-        // Bucle que muestra los multiplos de 7 menores que 100
 
         System.out.println("Multiplos de 7 menores que 100");
-
-        for (int i = 1; i < 100; i++){
-
-            if( i % 7 == 0){
+        // Bucle que recoge los numeros de (1 - 99)
+        for (int i = 1; i < 100; i++) {
+            // Cada uno de estos números que dividido entre 7 de 0, será mostrado por pantalla
+            if (i % 7 == 0) {
                 System.out.println(i);
             }
         }
